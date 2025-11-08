48f08b04440c9b8868a04d3792763fa8cf5e8c00
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-11-09 02:24:15 +0700

    menambahkan opsi radio button untuk jenis kelamin pada Formulir

--- app/src/main/java/com/example/navigasilayout/view/Formulir.kt
+++ app/src/main/java/com/example/navigasilayout/view/Formulir.kt
@@ -103,3 +103,26 @@ fun Formulir(
                         modifier = Modifier.fillMaxWidth(),
                         label = { Text(text = "Isian nama lengkap") }
                     )
+                    Text(
+                        text = "JENIS KELAMIN",
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.onSurface
+                    )
+                    Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+                        genderOptions.forEach { text ->
+                            Row(
+                                modifier = Modifier.fillMaxWidth().padding(horizontal = 4.dp),
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                RadioButton(
+                                    selected = text == selectedGender,
+                                    onClick = null
+                                )
+                                Text(
+                                    text = text,
+                                    style = MaterialTheme.typography.bodyLarge,
+                                    modifier = Modifier.padding(10.dp)
+                                )
+                            }
+                        }
+                    }
