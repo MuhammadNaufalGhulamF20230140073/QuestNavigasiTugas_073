7adec8eb6391906cba93ac698cedf8f855c35702
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-11-09 01:57:05 +0700

    menambahkan enum Navigasi dan impor komponen navigasi

---
+++ app/src/main/java/com/example/navigasilayout/Navigasi.kt
@@ -0,0 +1,20 @@
+package com.example.navigasilayout
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.navigasilayout.view.Formulir
+import com.example.navigasilayout.view.HomeScreen
+import com.example.navigasilayout.view.TampilData
+
+enum class Navigasi {
+    HomeScreen,
+    DataPeserta,
+    Formulir
+}
