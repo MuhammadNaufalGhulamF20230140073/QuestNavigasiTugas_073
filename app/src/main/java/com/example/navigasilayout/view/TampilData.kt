8d443cf0daf1e15ea45a8aee3659bfc1fa57cbb4
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-11-09 02:17:53 +0700

    menambahkan composable TampilData dengan daftar informasi

--- app/src/main/java/com/example/navigasilayout/view/TampilData.kt
+++ app/src/main/java/com/example/navigasilayout/view/TampilData.kt
@@ -1,2 +1,52 @@
 package com.example.navigasilayout.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavController
+import com.example.navigasilayout.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+
+@Composable
+fun TampilData(
+    modifier: Modifier = Modifier,
+    OnToFormBtnClick: () -> Unit,
+    OnToHomeBtnClick: () -> Unit,
+) {
+    val items = listOf(
+        stringResource(id = R.string.nama_lengkap) to "Muhammad Naufal Ghulam F",
+        stringResource(id = R.string.jenis_kelamin) to "Laki-Laki",
+        stringResource(id = R.string.alamat) to "Gedriyan"
+    )
